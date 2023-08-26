1edacd4a7fc641d858c218ef96a29003f435a68b
Anibal Vielma <asenciovd@gmail.com>
2023-08-25 21:02:34 -0400

    counter jsx

---
+++ src/Pages/Counter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    state = { counter: 0 };
+    incrementCounter = () => {
+        this.setState({ counter: this.state.counter + 1 });
+    };
+    render() {
+        const colorStyle = {
+            color: this.props.color,
+            fontSize: this.props.size + "px",
+        };
+        return (
+            <div style={colorStyle}>
+                React Component
+                <br />
+                <br />
+                <button onClick={this.incrementCounter}>Click Me!</button>
+                <br />
+                <br />
+                {this.state.counter}
+            </div>
+        );
+    }
+}
+export default Counter;
